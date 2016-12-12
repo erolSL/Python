0bbdfe09825c265a50382749c277aec417819714
erolSL <erol.uslu@bil.omu.edu.tr>
2016-12-12 16:44:08 +0300

    Execptions eklendi
    Execptions eklendi

--- Ev/version 0.001/Libs/ortakMethodlar.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-def hataBas(errMessage):
-    errFile = open("../log.txt", encoding='utf-8', mode='a')
-    errFile.write("%s %s" % (datetime.now(), errMessage + "\n"))
-    errFile.close()
--- Ev/version 0.003/Libs/ortakMethodlar.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-logFile = "../log.txt"
-
-def hataBas(errMessage):
-    errFile = open(logFile, encoding='utf-8', mode='a')
-    errFile.write("%s %s" % (datetime.now(), errMessage + "\n"))
-    errFile.close()
